712c2faa5fe24fc8072bf7490f69c303943b90c1
natraj <sysad@trioangle.com>
2018-06-17 13:24:13 +0530

    test code climate 1- natraj

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -19,6 +19,9 @@ trait CreatesApplication
         $app->make(Kernel::class)->bootstrap();
 
         Hash driver('bcrypt')->setRounds(4);
+        lndflksdv sd;lfk[runkit_return_value_used()s;for ($i=0; $i < ; $i++) { 
+            # code...
+        }]
 
         return $app;
     }
